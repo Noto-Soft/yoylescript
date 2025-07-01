9d246349c63dcb7f20f9bb77a80e4399a9aefbc9
bismuthnickel <warreneverettstevens@gmail.com>
2025-07-01 17:21:36 -0400

    documentation kinda

--- src/inc/yoyleerr.h
+++
@@ -1,6 +0,0 @@
-#pragma once
-
-__attribute__((noreturn))
-void fatal(const char *msg);
-__attribute__((noreturn))
-void fatalf(const char *fmt, ...);
--- src/inc/yoylestate.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-
-typedef struct varentry_t varentry_t;
-typedef struct yoylestack_t yoylestack_t;
-
-typedef struct yoylestate_t {
-    varentry_t* symbol_table;
-    yoylestack_t* arg_stack;
-} yoylestate_t;
